b9c2c03f2d544a9627b0173b14c64c86323be8fb
Marcelo <marceloamaral1364@gmail.com>
2023-02-20 10:26:22 -0300

    Dia 3

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,12 +4,16 @@ print('-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
 
 numero_secreto = 42
 tentativas = 3
-rodada = 1
 
-while rodada <= tentativas:
+for rodada in range(1, tentativas + 1):
+
     print(f'Tentativa {rodada} de {tentativas}')
-    chute = int(input('Digite o seu número: '))
+    chute = int(input('Digite um número entre 1 e 100: '))
     print('Você digitou', chute)
+
+    if chute < 1 or chute > 100:
+        print('Você deve digitar um número entre 1 e 100')
+        continue
 
     acertou = numero_secreto == chute
     maior = chute > numero_secreto
@@ -23,6 +27,6 @@ while rodada <= tentativas:
             print('Errou! Seu chute foi maior que o número secreto.')
         elif menor:
             print('Errou! Seu chute foi menor que o número secreto.')
-    rodada += 1
+
 
 print('Fim do jogo!')
