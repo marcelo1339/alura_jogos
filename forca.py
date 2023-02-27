79d669b2ccace1197e2ab95ecc56023766a6b795
Marcelo <marceloamaral1364@gmail.com>
2023-02-27 12:16:03 -0300

    Dia 1

--- forca.py
+++ forca.py
@@ -9,8 +9,14 @@ def jogar_forca():
     palavra_secreta = 'banana'
 
     while not perdeu or not ganhou:
-        print('Jogando...')
+        chute = str(input('Qual letra? ')).strip().lower()
+        index = 0
 
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                print(f'A letra "{letra}", está na posição {index}')
+
+            index += 1
 
     print('Fim do jogo!')
 
