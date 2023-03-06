759a5362a8a050a72bed32fcfa0e1a1f1dcb487d
Marcelo <marceloamaral1364@gmail.com>
2023-03-06 13:01:34 -0300

    Dia 3

--- forca.py
+++ forca.py
@@ -1,26 +1,40 @@
 
 def jogar_forca():
     print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-    print("Bem vindo ao jogo da forca")
+    print("   Bem vindo ao jogo da forca")
     print('-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
 
     perdeu = ganhou = False
 
-    palavra_secreta = 'banana'
-    letras_acertadas = ['_', '_', '_', '_', '_', '_']
+    palavra_secreta = 'maçã'.upper()
+    letras_acertadas = []
 
-    while not perdeu or not ganhou:
-        chute = str(input('Qual letra? ')).strip().lower()
-        index = 0
+    for letra in palavra_secreta:
+        letras_acertadas.append('_')
 
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                letras_acertadas[index] = chute
+    tentativas = erros = 0
 
-            index += 1
+    while not perdeu and not ganhou:
+        print(f'Tentativa {tentativas + 1} de 6')
+
+        chute = str(input('Tente uma letra? ')).strip().upper()
+
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = chute
+                index += 1
+        elif chute not in palavra_secreta:
+            tentativas += 1
 
         print(letras_acertadas)
 
+        perdeu = tentativas == 6
+        ganhou = '_' not in letras_acertadas
+
+        if ganhou:
+            print('Você ganhou!!')
 
     print('Fim do jogo!')
 
